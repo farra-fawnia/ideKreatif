1bf4e62060f5f0b62d1382250c923700a47ed9af
farra-fawnia <farrafawnia01@gmail.com>
2025-02-03 10:57:26 +0700

    Menambahkan Kode-kode pada init_session.php

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -3,9 +3,16 @@ session_start();
 
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
-
 //ambil notifikasi jika ada, kemudian hapus dari sesi
 $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
+}
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+$_SESSION['notification'] = [
+    'type' => 'danger',
+    'message' => 'Silahkan Login Terlebih Dahulu!'
+];
+header('Location: ./auth/login.php');
+exit();
 }
